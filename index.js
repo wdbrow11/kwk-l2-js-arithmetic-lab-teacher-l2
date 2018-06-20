60ce7858968e0cc1881c4e0492a3c002e3733f86
wdbrow11 <will.brown.hc@gmail.com>
2018-06-20 08:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random(0,20);
 let randomInteger = Math.floor(randomNumber);
-let randomUserID = 
+let randomUserID = randomInteger + 1000000001
